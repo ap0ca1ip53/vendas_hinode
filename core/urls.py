297b461344bb5042dd1b949f0c053838b1c0e831
ap0ca1ip53 <jfacardoso@gmail.com>
2016-10-25 18:45:47 -0300

    Aplicando novos templates

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 app_name = 'hinode'
 urlpatterns = [
-    url(r'^index/$', views.home, name="home"),
+    url(r'^index/$', views.index, name="index"),
     url(r'^login/$', views.login_view, name="login"),
     url(r'^logout/$', views.logout_view, name="logout"),
+    url(r'^clientes/$', views.ClienteList.as_view(), name='clientes_list'),
+    url(r'^clientes/add/$', views.ClienteCreate.as_view(), name='clientes_add'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -3,21 +3,35 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.views import login, logout
-from django.core.urlresolvers import reverse_lazy, reverse
+from django.core.urlresolvers import reverse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from core.models import Cliente
 
-def home(request):
+
+def index(request):
     return render(request, 'core/home.html')
 
 
 def login_view(request, *args, **kwargs):
     if request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('hinode:home'))
+        return HttpResponseRedirect(reverse('hinode:index'))
 
-    kwargs['extra_context'] = {'next': 'hinode:home'}
+    kwargs['extra_context'] = {'next': 'hinode:index'}
     kwargs['template_name'] = 'core/login.html'
     return login(request, *args, **kwargs)
 
 
 def logout_view(request, *args, **kwargs):
-    kwargs['next_page'] = reverse('hinode:home')
+    kwargs['next_page'] = reverse('hinode:index')
     return logout(request, *args, **kwargs)
+
+class ClienteList(ListView):
+    model = Cliente
+    template = 'core/cliente_list.html'
+
+class ClienteCreate(CreateView):
+    model = Cliente
+    fields = ['nome', 'sexo']
+    form_class = 
